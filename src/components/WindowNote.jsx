9207346eb172dfede59a51515c147a92a98b4c56
Adonislan Alves <adonislan_alves@outlook.com>
2024-08-24 11:50:22 -0300

    ajustes  no layout do modal

--- src/components/WindowNote.jsx
+++ src/components/WindowNote.jsx
@@ -13,23 +13,58 @@ export default function WindowNote({ isOpen, onClose }) {
 
   return (
     <section className="modal">
-      <div className="modal-container">
+      <form className="modal-container" onSubmit={handleSubmit}>
         <span className="close" onClick={onClose}>
           &times; {/* Símbolo de fechar */}
         </span>
         <h2 className="title-modal">Adicionar Tarefa:</h2>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="reminder-text">Lembrete:</label>
-          <div>
-            <textarea
-              id="reminder-text"
-              rows="4"
-              placeholder="Digite seu lembrete aqui..."
-            ></textarea>
-          </div>
-          <Btn type="submit" id="btn" name="Enviar" />
-        </form>
-      </div>
+        <div className="area-title">
+          <label htmlFor="title">Título:</label>
+          <input type="text" name="title" id="title" />
+        </div>
+
+        <label htmlFor="reminder-text">Lembrete:</label>
+        <div>
+          <textarea
+            id="reminder-text"
+            rows="4"
+            placeholder="Digite seu lembrete aqui..."
+          ></textarea>
+        </div>
+
+        <div className="area-radios">
+          <input
+            type="radio"
+            id="alta-prioridade"
+            name="prioridade"
+            value="Alta"
+            className="radios"
+          />
+          <label htmlFor="alta-prioridade">ALTA PRIORIDADE</label>
+          <br />
+
+          <input
+            type="radio"
+            id="media-prioridade"
+            name="prioridade"
+            value="Média"
+            className="radios"
+          />
+          <label htmlFor="media-prioridade">PRIORIDADE MÉDIA</label>
+          <br />
+
+          <input
+            type="radio"
+            id="baixa-prioridade"
+            name="prioridade"
+            value="Baixa"
+            className="radios"
+          />
+          <label htmlFor="baixa-prioridade">BAIXA PRIORIDADE</label>
+        </div>
+
+        <Btn type="submit" id="btn" name="Enviar" />
+      </form>
     </section>
   );
 }
